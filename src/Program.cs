43e3373624d78f7f12ec71cd8d1c99c8ebb1b236
juliobugfixer <ngmanuel21@gmail.com>
2026-08-08 00:52:55 +0100

    feat: adiciona função menu

--- src/Program.cs
+++ src/Program.cs
@@ -1,2 +1,14 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿using System.Linq.Expressions;
+
+Menu();
+
+static void Menu()
+{
+    Console.WriteLine("****Escolha a unidade********");
+    Console.WriteLine("1- Km \n2- hm \n3- dam \n4- m \n5- dm \n6- cm \n7- mm \n0 - Sair");
+
+    Console.WriteLine("Digite a opção correspondente: ");
+    int opcao = Convert.ToInt32(Console.ReadLine());
+
+
+}
