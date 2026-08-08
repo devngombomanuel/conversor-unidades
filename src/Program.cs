ad3eee9ab7bd6e2fdc355a2d3bec6501d979deff
juliobugfixer <ngmanuel21@gmail.com>
2026-08-08 01:50:23 +0100

    feat: Adiciona funcao para dam e dm

--- src/Program.cs
+++ src/Program.cs
@@ -9,8 +9,8 @@ static void Menu()
     double valor = Convert.ToDouble(Console.ReadLine());
     Console.WriteLine("\n\n****Escolha a unidade****" +
         "\n1- Km \n2- hm \n3- dam " +
-        "\n4- m \n5- dm \n6- cm " +
-        "\n7- mm \n0 - Sair");
+        "\n4- dm \n5- cm \n6- mm " +
+        "\n0 - Sair");
     Console.Write("Digite a opção correspondente: ");
     int opcao = Convert.ToInt32(Console.ReadLine());
     Console.BackgroundColor= ConsoleColor.Blue;
@@ -19,6 +19,9 @@ static void Menu()
     {
         case 0: System.Environment.Exit(0); break;
         case 1: Console.WriteLine($"{valor} m corresponde a  {Kilometro(valor)} Km"); break;
+        case 2: Console.WriteLine($"{valor} m corresponde a  {Hectometro(valor)} hm"); break;
+        case 3: Console.WriteLine($"{valor} m corresponde a  {Dacametro(valor)} dam"); break;
+        case 4: Console.WriteLine($"{valor} m corresponde a  {Decimetro(valor)} dm"); break;
     }
 }
 
@@ -30,6 +33,16 @@ static double Hectometro(double valor)
 {
     return (valor / 100);
 }
+static double Dacametro(double valor)
+{
+    return (valor / 10);
+}
+static double Decimetro(double valor)
+{
+    return (valor * 10);
+}
+
+
 
 Console.ResetColor();
 
