19f1163d99af11221629ae0f33cdc486678b95f5
juliobugfixer <ngmanuel21@gmail.com>
2026-08-08 01:44:20 +0100

    feat: Adiciona mudança de cor de fundo e texto

--- src/Program.cs
+++ src/Program.cs
@@ -7,23 +7,18 @@ static void Menu()
     Console.WriteLine("****Bem-vindo****");
     Console.Write("Digite o valor [metro]: ");
     double valor = Convert.ToDouble(Console.ReadLine());
-    Console.WriteLine("****Escolha a unidade****" +
+    Console.WriteLine("\n\n****Escolha a unidade****" +
         "\n1- Km \n2- hm \n3- dam " +
         "\n4- m \n5- dm \n6- cm " +
         "\n7- mm \n0 - Sair");
     Console.Write("Digite a opção correspondente: ");
     int opcao = Convert.ToInt32(Console.ReadLine());
-
+    Console.BackgroundColor= ConsoleColor.Blue;
+    Console.ForegroundColor= ConsoleColor.White;
     switch (opcao)
     {
         case 0: System.Environment.Exit(0); break;
-        case 1: ; break;
-        case 2: ; break;
-        case 3: ; break;
-        case 4: ; break;
-        case 5: ; break;
-        case 6: ; break;
-        case 7: ; break;
+        case 1: Console.WriteLine($"{valor} m corresponde a  {Kilometro(valor)} Km"); break;
     }
 }
 
